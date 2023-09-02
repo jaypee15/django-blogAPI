7396824da8088b19e2fc26a233326a1c64a6c38f
jaypee15 <okoyejohnpaul15@gmail.com>
2023-09-02 22:13:06 +0100

    changed views to viewsets and routers

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model 
 from rest_framework import serializers
+
 
 from .models import Post 
 
@@ -12,3 +14,8 @@ class PostSerializer(serializers.ModelSerializer):
 			"body",
 			"created_at",
 			)
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = get_user_model()
+		fields = ("id", "username",)
