e6455d01fc39f11f1b5f435c37e2c0ecb53e6807
jaypee15 <okoyejohnpaul15@gmail.com>
2023-09-02 20:18:34 +0100

    added user authentication with dj-rest-auth

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,10 +37,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
 
     # 3rd-party
     "rest_framework",
     "corsheaders",
+    "rest_framework.authtoken",
+    "allauth", 
+    "allauth.account", 
+    "allauth.socialaccount", 
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
 
     # local
     "accounts.apps.AccountsConfig",
@@ -71,6 +78,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
             ],
         },
     },
@@ -133,10 +141,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-REST_FRAMEWORK = { 
+REST_FRAMEWORK = {
 "DEFAULT_PERMISSION_CLASSES": [
 "rest_framework.permissions.IsAuthenticated",
 ],
+"DEFAULT_AUTHENTICATION_CLASSES": [ 
+"rest_framework.authentication.SessionAuthentication",
+"rest_framework.authentication.TokenAuthentication",],
 }
 
 CORS_ORIGIN_WHITELIST = (
@@ -145,3 +156,7 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" 
+
+SITE_ID = 1 
